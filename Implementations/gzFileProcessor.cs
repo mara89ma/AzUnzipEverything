281327f82dec4b0fca32c9f90cc61c8b08359278
Suzie/DevSecOps/Cloud/@Cloocus <84659725+mara89ma@users.noreply.github.com>
2023-11-06 12:58:26 +0900

    Update gzFileProcessor.cs

--- Implementations/gzFileProcessor.cs
+++ Implementations/gzFileProcessor.cs
@@ -1,53 +1,44 @@
+using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 using System.IO.Compression;
-using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
-public class GZipFileProcessor
+namespace AzUnzipEverything.Implementations
 {
-    private readonly string _destinationPath;
-    private readonly ILogger _logger;
-
-    public GZipFileProcessor(string destinationPath, ILogger logger)
+    public class GZipFileProcessor : FileProcessorBase
     {
-        _destinationPath = destinationPath;
-        _logger = logger;
-    }
+        private readonly ILogger<FileProcessorBase> _logger;
 
-    public byte[] ProcessFile(Stream blobStream)
-    {
-        try
+        public GZipFileProcessor(CloudBlobContainer destinationContainer, SecretSettings secretSettings, ILogger<FileProcessorBase> logger) : base(destinationContainer, logger)
         {
-            _logger.LogInformation("Attempting GZIP extraction...");
-
-            using (MemoryStream decompressedStream = new MemoryStream())
-            {
-                using (GZipStream decompressionStream = new GZipStream(blobStream, CompressionMode.Decompress))
-                {
-                    decompressionStream.CopyTo(decompressedStream);
-                }
-
-                // You can further process the decompressed data here
-                // For example, you can save it to a file or perform other operations.
-                // To access the byte array, use: decompressedStream.ToArray()
-
-                _logger.LogInformation("GZIP extraction completed successfully.");
-                return decompressedStream.ToArray(); // Return decompressed data if needed
-            }
+            _logger = logger;
         }
-        catch (Exception ex)
+
+        public override async Task ProcessFile(Stream blobStream)
         {
-            _logger.LogError($"Error during GZIP extraction: {ex.Message}");
-            throw; // Propagate the exception
+            try
+            {
+                _logger.LogInformation("Attempting GZIP extraction...");
+
+                using (MemoryStream decompressedStream = new MemoryStream())
+                {
+                    using (GZipStream decompressionStream = new GZipStream(blobStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedStream);
+                    }
+
+                    // You can further process the decompressed data here
+                    // For example, save it to a file or perform other operations
+
+                    _logger.LogInformation("GZIP extraction completed successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error during GZIP extraction: {ex.Message}");
+                throw; // Propagate the exception
+            }
         }
     }
 }
-
-// Usage example:
-// Assuming you have the necessary settings (destination path and logger) initialized:
-// var gzipProcessor = new GZipFileProcessor("/path/to/destination", logger);
-// using (FileStream blobStream = File.OpenRead("your_gzip_file.gz"))
-// {
-//     byte[] decompressedData = gzipProcessor.ProcessFile(blobStream);
-//     // Use the decompressedData byte array as needed
-// }
