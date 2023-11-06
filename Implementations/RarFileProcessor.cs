efe715acbb1be678c4b839909d83b405ca4275ba
Suzie/DevSecOps/Cloud/@Cloocus <84659725+mara89ma@users.noreply.github.com>
2023-11-06 13:44:03 +0900

    Update RarFileProcessor.cs
    gz

--- Implementations/RarFileProcessor.cs
+++ Implementations/RarFileProcessor.cs
@@ -1,22 +1,21 @@
-﻿using AzUnzipEverything.Abstractions;
+using AzUnzipEverything.Abstractions;
 using AzUnzipEverything.Infrastructure.Settings;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Blob;
-using SharpCompress.Archives.Rar;
+using SharpCompress.Archives;
 using SharpCompress.Common;
-using SharpCompress.Readers;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AzUnzipEverything.Implementations
 {
-    public class RarFileProcessor : FileProcessorBase
+    public class GZipFileProcessor : FileProcessorBase
     {
         private readonly SecretSettings _secretSettings;
         private readonly ILogger<FileProcessorBase> _logger;
 
-        public RarFileProcessor(CloudBlobContainer destinationContainer, SecretSettings secretSettings, ILogger<FileProcessorBase> logger) : base(destinationContainer, logger)
+        public GZipFileProcessor(CloudBlobContainer destinationContainer, SecretSettings secretSettings, ILogger<FileProcessorBase> logger) : base(destinationContainer, logger)
         {
             _secretSettings = secretSettings;
             _logger = logger;
@@ -24,19 +23,19 @@ namespace AzUnzipEverything.Implementations
 
         public override async Task ProcessFile(Stream blobStream)
         {
-            var rarReaderOptions = new ReaderOptions()
+            var gzipOptions = new ReaderOptions()
             {
                 ArchiveEncoding = new ArchiveEncoding(Encoding.UTF8, Encoding.UTF8),
                 Password = _secretSettings?.ZipPassword,
                 LookForHeader = true
             };
 
-            if (RarArchive.IsRarFile(blobStream, rarReaderOptions))
+            if (GZipArchive.IsGZipFile(blobStream))
             {
-                _logger.LogInformation("Blob is a rar file; beginning extraction....");
+                _logger.LogInformation("Blob is a GZip file; beginning extraction....");
                 blobStream.Position = 0;
 
-                using var reader = RarArchive.Open(blobStream, rarReaderOptions);
+                using var reader = GZipArchive.Open(blobStream, gzipOptions);
 
                 await ExtractArchiveFiles(reader.Entries);
             }
