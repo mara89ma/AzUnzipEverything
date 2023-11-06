8af17887828ab4a4cdf9bccceed1873087e90def
Suzie/DevSecOps/Cloud/@Cloocus <84659725+mara89ma@users.noreply.github.com>
2023-11-06 13:27:33 +0900

    Update gzFileProcessor.cs

--- Implementations/gzFileProcessor.cs
+++ Implementations/gzFileProcessor.cs
@@ -1,44 +1,66 @@
-using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 using System.IO.Compression;
-using System.Threading.Tasks;
 
-namespace AzUnzipEverything.Implementations
+public class FileCompressionModeExample
 {
-    public class GZipFileProcessor : FileProcessorBase
+    private const string Message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    private const string OriginalFileName = "original.txt";
+    private const string CompressedFileName = "compressed.gz";
+    private const string DecompressedFileName = "decompressed.txt";
+
+    public static void Main()
     {
-        private readonly ILogger<FileProcessorBase> _logger;
+        CreateFileToCompress();
+        CompressFile();
+        DecompressFile();
+        PrintResults();
+        DeleteFiles();
 
-        public GZipFileProcessor(CloudBlobContainer destinationContainer, SecretSettings secretSettings, ILogger<FileProcessorBase> logger) : base(destinationContainer, logger)
-        {
-            _logger = logger;
-        }
+        /*
+         Output:
 
-        public override async Task ProcessFile(Stream blobStream)
-        {
-            try
-            {
-                _logger.LogInformation("Attempting GZIP extraction...");
+            The original file 'original.txt' weighs 445 bytes. Contents: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-                using (MemoryStream decompressedStream = new MemoryStream())
-                {
-                    using (GZipStream decompressionStream = new GZipStream(blobStream, CompressionMode.Decompress))
-                    {
-                        decompressionStream.CopyTo(decompressedStream);
-                    }
+            The compressed file 'compressed.gz' weighs 283 bytes.
 
-                    // You can further process the decompressed data here
-                    // For example, save it to a file or perform other operations
+            The decompressed file 'decompressed.txt' weighs 445 bytes. Contents: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+         */
+    }
 
-                    _logger.LogInformation("GZIP extraction completed successfully.");
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error during GZIP extraction: {ex.Message}");
-                throw; // Propagate the exception
-            }
-        }
+    private static void CreateFileToCompress() => File.WriteAllText(OriginalFileName, Message);
+
+    private static void CompressFile()
+    {
+        using FileStream originalFileStream = File.Open(OriginalFileName, FileMode.Open);
+        using FileStream compressedFileStream = File.Create(CompressedFileName);
+        using var compressor = new GZipStream(compressedFileStream, CompressionMode.Compress);
+        originalFileStream.CopyTo(compressor);
+    }
+
+    private static void DecompressFile()
+    {
+        using FileStream compressedFileStream = File.Open(CompressedFileName, FileMode.Open);
+        using FileStream outputFileStream = File.Create(DecompressedFileName);
+        using var decompressor = new GZipStream(compressedFileStream, CompressionMode.Decompress);
+        decompressor.CopyTo(outputFileStream);
+    }
+
+    private static void PrintResults()
+    {
+        long originalSize = new FileInfo(OriginalFileName).Length;
+        long compressedSize = new FileInfo(CompressedFileName).Length;
+        long decompressedSize = new FileInfo(DecompressedFileName).Length;
+
+        Console.WriteLine($"The original file '{OriginalFileName}' weighs {originalSize} bytes. Contents: \"{File.ReadAllText(OriginalFileName)}\"");
+        Console.WriteLine($"The compressed file '{CompressedFileName}' weighs {compressedSize} bytes.");
+        Console.WriteLine($"The decompressed file '{DecompressedFileName}' weighs {decompressedSize} bytes. Contents: \"{File.ReadAllText(DecompressedFileName)}\"");
+    }
+
+    private static void DeleteFiles()
+    {
+        File.Delete(OriginalFileName);
+        File.Delete(CompressedFileName);
+        File.Delete(DecompressedFileName);
     }
 }
